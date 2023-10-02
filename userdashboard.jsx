34bf667ec82f24a51ba7b325daceb901b7647bed
Satyam-Tiwari11 <144764776+Satyam-Tiwari11@users.noreply.github.com>
2023-10-02 14:19:04 +0530

    Update userdashboard.jsx

--- userdashboard.jsx
+++ userdashboard.jsx
@@ -33,3 +33,55 @@ function UserDashboard() {
 }
 
 export default UserDashboard;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React from "react";
+import "./UserDashboard.css";
+import { useNavigate } from "react-router-dom";
+
+function UserDashboard() {
+  const navigate = useNavigate();
+  const handleViewLoans = () => {
+    navigate("/fetch");
+  };
+  const handleApplyLoan = () => {
+    navigate("/applyLoan");
+  };
+  const handleViewItems = () => {
+    navigate("/itemfetch");
+  };
+  const employeeID = sessionStorage.getItem("employeeID");
+
+  return (
+    <div className="user-dashboard">
+      <h1>User Dashboard</h1>
+      <div className="button-container">
+        <button className="dashboard-button" onClick={handleViewLoans}>
+          View Loans
+        </button>
+        <button className="dashboard-button" onClick={handleApplyLoan}>
+          Apply for Loan
+        </button>
+        <button className="dashboard-button" onClick={handleViewItems}>
+          View Items Purchased
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default UserDashboard;
+
+
